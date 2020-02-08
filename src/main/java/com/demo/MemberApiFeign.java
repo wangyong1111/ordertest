36b9cb1f22da569e5ad906af6080477ca42418b6
wangyong1111 <39075986+wangyong1111@users.noreply.github.com>
2020-02-08 12:12:04 +0000

    update
    sadfa

--- src/main/java/com/demo/MemberApiFeign.java
+++ src/main/java/com/demo/MemberApiFeign.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * Created by wangyong on 2019/8/30.
  * 111111111111111111111111111111
+ *22222222222222222222
  */
 @FeignClient(name="app-itmayiedu-member")
 public interface MemberApiFeign {
